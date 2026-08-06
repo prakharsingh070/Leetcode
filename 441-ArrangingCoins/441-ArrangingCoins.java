de839358ce32392c89df2f15832273eecd069491
Prakhar Pratap Singh <pratapprakhar070@gmail.com>
2026-08-06 12:17:28 +0530

    Create 441-ArrangingCoins

---
+++ 441-ArrangingCoins/441-ArrangingCoins.java
@@ -0,0 +1,16 @@
+// Last updated: 8/6/2026, 12:17:30 PM
+class Solution {
+    public int arrangeCoins(int n) {
+        long left=1;
+        long right=n;
+        while(left<=right){
+            long mid = left + (right-left)/2;
+            long coins = mid*(mid+1)/2;
+            if(coins==n) return(int) mid;
+            else if(coins<n) left = mid+1;
+            else right = mid-1;
+
+        }
+        return (int)right;
+    }
+}
