d80d9f54c38cd0abdada2133ac5bae4b1af6da76
Prakhar Pratap Singh <pratapprakhar070@gmail.com>
2026-08-06 12:18:39 +0530

    Create 191-NumberOf1Bits

---
+++ 191-NumberOf1Bits/191-NumberOf1Bits.java
@@ -0,0 +1,14 @@
+// Last updated: 8/6/2026, 12:18:40 PM
+class Solution {
+    public int hammingWeight(int n) {
+        int count =0 ;
+        int mask = 1;
+        for(int i=0;i<32;i++){
+            if((n & mask)!=0){
+                count++;
+            }
+            mask<<=1;
+        }
+        return count;
+    }
+}
