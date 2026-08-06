0136724696e1ed67ec0e7c82677becd6c3945e3d
Prakhar Pratap Singh <pratapprakhar070@gmail.com>
2026-08-06 12:16:06 +0530

    Create 861-FlippingAnImage

---
+++ 861-FlippingAnImage/861-FlippingAnImage.java
@@ -0,0 +1,12 @@
+// Last updated: 8/6/2026, 12:16:08 PM
+class Solution {
+    public int[][] flipAndInvertImage(int[][] image) {
+        for(int[] row:image){
+       for(int i=0;i<(image[0].length+1)/2;i++){
+        int temp=row[i]^1;
+        row[i]=row[image[0].length-i-1]^1;
+        row[image[0].length-i-1]=temp;
+       } 
+    }
+return image;
+}}
