0133d182f057c9253590b7f89f210faf28849e84
Prakhar Pratap Singh <pratapprakhar070@gmail.com>
2026-08-06 12:15:25 +0530

    Create 1245-UserActivityForThePast30DaysI

---
+++ 1245-UserActivityForThePast30DaysI/1245-UserActivityForThePast30DaysI.sql
@@ -0,0 +1,2 @@
+-- Last updated: 8/6/2026, 12:15:27 PM
+select activity_date as day , count(distinct user_id) as active_users from Activity where activity_date between  '2019-06-28' and '2019-07-27'group by activity_date;
