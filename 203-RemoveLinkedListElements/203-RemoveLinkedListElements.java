abdf472bebe31850d2c01c69df26ad9aade912ef
Prakhar Pratap Singh <pratapprakhar070@gmail.com>
2026-08-16 21:51:33 +0530

    File updated at 8/16/2026, 9:51:33 PM

--- 203-RemoveLinkedListElements/203-RemoveLinkedListElements.java
+++ 203-RemoveLinkedListElements/203-RemoveLinkedListElements.java
@@ -1,32 +1,25 @@
-// Last updated: 8/6/2026, 12:18:24 PM
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
-    public ListNode removeElements(ListNode head, int val) {
-        while(head!=null && head.val==val){
-            head=head.next;
-        }
-        if(head==null){ 
-            return null;
-        }
-        ListNode curr=head;
-            while(curr.next!=null){
-                if(curr.next.val==val){
-                // If the next node needs to be removed,
-                // skip it by linking to the node after it.
-                     curr.next=curr.next.next;
-                }else{
-                    curr=curr.next; // Move forward only when no deletion occurs.
-                }
-            }
-        return head;
-    }
-}
+// Last updated: 8/16/2026, 9:51:33 PM
+1/**
+2 * Definition for singly-linked list.
+3 * public class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode() {}
+7 *     ListNode(int val) { this.val = val; }
+8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+9 * }
+10 */
+11class Solution {
+12    public ListNode removeElements(ListNode head, int val) {
+13        while(head!=null && head.val==val){
+14            head = head.next;
+15        }
+16        if(head==null) return null;
+17        ListNode curr = head;
+18        while(curr.next!=null){
+19            if(curr.next.val==val) curr.next= curr.next.next;
+20            else curr = curr.next;
+21        }
+22        return head;
+23    }
+24}
