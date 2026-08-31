1a46d7034f772050ab1e42b57fc0bd48f2bc1c74
Prakhar Pratap Singh <pratapprakhar070@gmail.com>
2026-08-31 21:37:09 +0530

    File updated at 8/31/2026, 9:37:10 PM

--- 724-FindPivotIndex/724-FindPivotIndex.java
+++ 724-FindPivotIndex/724-FindPivotIndex.java
@@ -1,20 +1,12 @@
-// Last updated: 8/31/2026, 1:30:19 PM
+// Last updated: 8/31/2026, 9:37:10 PM
 1class Solution {
-2    public int pivotIndex(int[] nums) {
-3        int n = nums.length;
-4        int[] lsum=new int[n];
-5        int[] rsum = new int[n];
-6        lsum[0]=0;
-7        rsum[n-1] = 0;
-8        for(int i=1;i<n;i++){
-9            lsum[i] = lsum[i-1]+nums[i-1];
-10        }
-11        for(int i=n-2;i>=0;i--){
-12            rsum[i] = rsum[i+1]+nums[i+1];
-13        }
-14        for(int i=0;i<n;i++){
-15            if(lsum[i]==rsum[i]) return i;
-16        }
-17        return -1;
-18    }
-19}
+2    public int findPoisonedDuration(int[] timeSeries, int duration) {
+3        int n = timeSeries.length;
+4        int t=0;
+5        for(int i=1;i<n;i++){
+6            t += Math.min(duration,timeSeries[i]-timeSeries[i-1]);
+7        }
+8        t+=duration;
+9        return t;
+10    }
+11}
