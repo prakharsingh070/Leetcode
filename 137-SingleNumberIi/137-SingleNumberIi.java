7a5f7296a22864cb811a5bee3280a3d5e7219715
Prakhar Pratap Singh <pratapprakhar070@gmail.com>
2026-08-31 13:32:59 +0530

    Create 137-SingleNumberIi

---
+++ 137-SingleNumberIi/137-SingleNumberIi.java
@@ -0,0 +1,12 @@
+// Last updated: 8/31/2026, 1:32:59 PM
+class Solution {
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        for (int i=0;i<nums.length-1; i+= 3) {
+            if(nums[i] != nums[i+1]) {
+                return nums[i];
+            }
+        }
+        return nums[nums.length-1];
+    }
+}
