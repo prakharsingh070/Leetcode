044b852cc9800924d2555cdfdf8caab4ad983a6c
Prakhar Pratap Singh <pratapprakhar070@gmail.com>
2026-09-04 16:52:30 +0530

    Create 921-MinimumAddToMakeParenthesesValid

---
+++ 921-MinimumAddToMakeParenthesesValid/921-MinimumAddToMakeParenthesesValid.java
@@ -0,0 +1,28 @@
+// Last updated: 9/4/2026, 4:52:30 PM
+class Solution {
+    public int minAddToMakeValid(String s) {
+        // Try solving this in O(1) space (without using a Stack class)
+        Stack<Character> st = new Stack<>();
+        st.push(s.charAt(0));
+        for(int i=1;i<s.length();i++){
+            char c = s.charAt(i);
+            if(st.isEmpty()){
+                st.push(c);
+                continue;
+            }
+            if(st.peek()=='(' && c==')'){
+                st.pop();
+            }
+            else{
+                st.push(c);
+            }
+        }
+        int c=0;
+        while(!st.isEmpty()){
+            c++;
+            st.pop();
+        }
+        return c;
+        
+    }
+}
