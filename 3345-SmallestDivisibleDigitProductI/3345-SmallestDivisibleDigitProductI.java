6e0cefb7d99db286c3207fce336e3e5ecd9ffe43
Prakhar Pratap Singh <pratapprakhar070@gmail.com>
2026-09-04 16:50:11 +0530

    Create 3345-SmallestDivisibleDigitProductI

---
+++ 3345-SmallestDivisibleDigitProductI/3345-SmallestDivisibleDigitProductI.java
@@ -0,0 +1,16 @@
+// Last updated: 9/4/2026, 4:50:11 PM
+class Solution {
+    public int smallestNumber(int n, int t) {
+        int q = n / 10, r = n % 10;
+
+        int req = t / gcd(q + (10 - q) / 10, t);
+        int nxt = ((r + req - 1) / req) * req;
+        int x = nxt - (nxt - 10) * (nxt / 10);
+
+        return q * 10 + x;
+    }
+
+    private int gcd(int a, int b) {
+        return (b == 0) ? a : gcd(b, a % b);
+    }
+}
