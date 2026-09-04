627a215a8278549f87920c855f8187c8eeab1529
Prakhar Pratap Singh <pratapprakhar070@gmail.com>
2026-09-04 16:51:47 +0530

    Create 1304-FindNUniqueIntegersSumUpToZero

---
+++ 1304-FindNUniqueIntegersSumUpToZero/1304-FindNUniqueIntegersSumUpToZero.java
@@ -0,0 +1,11 @@
+// Last updated: 9/4/2026, 4:51:47 PM
+class Solution {
+    public int[] sumZero(int n) {
+        int[] res = new int[n];
+        res[0] = n * (1 - n) / 2;
+        for (int i = 1; i < n; ++i)
+            res[i] = i;
+        return res;
+        
+    }
+}
