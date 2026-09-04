d02cffe3cfc1f3c5ce13b98f942cdae995e6abeb
Prakhar Pratap Singh <pratapprakhar070@gmail.com>
2026-09-04 16:51:01 +0530

    Create 1732-FindTheHighestAltitude

---
+++ 1732-FindTheHighestAltitude/1732-FindTheHighestAltitude.java
@@ -0,0 +1,14 @@
+// Last updated: 9/4/2026, 4:51:01 PM
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int curr = 0;
+        int maxAlt = 0;
+
+        for(int g : gain){
+            curr += g;
+            maxAlt = Math.max(maxAlt, curr);
+        }
+
+        return maxAlt;
+    }
+}
