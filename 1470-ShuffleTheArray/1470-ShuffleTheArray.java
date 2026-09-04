72bb0e1e4eb01ad3764e871d58754950e6bc8ae8
Prakhar Pratap Singh <pratapprakhar070@gmail.com>
2026-09-04 16:51:24 +0530

    Create 1470-ShuffleTheArray

---
+++ 1470-ShuffleTheArray/1470-ShuffleTheArray.java
@@ -0,0 +1,16 @@
+// Last updated: 9/4/2026, 4:51:24 PM
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int len = nums.length;
+        int[] ans = new int[len];
+        for(int i=0; i<len; i++){
+            if(i%2==0){
+                ans[i] = nums[i/2];
+            }
+            else{
+                ans[i] = nums[n+(i/2)];
+            }
+        }
+        return ans;
+    }
+}
