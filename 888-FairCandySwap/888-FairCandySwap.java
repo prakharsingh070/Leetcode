64d04b9a700bef506db0abf2064cdf911e1f3bec
Prakhar Pratap Singh <pratapprakhar070@gmail.com>
2026-09-04 16:52:33 +0530

    Create 888-FairCandySwap

---
+++ 888-FairCandySwap/888-FairCandySwap.java
@@ -0,0 +1,19 @@
+// Last updated: 9/4/2026, 4:52:33 PM
+class Solution {
+    public int[] fairCandySwap(int[] aliceSizes, int[] bobSizes) {
+        int sa=0,sb=0;
+        for(int a:aliceSizes) sa+=a;
+        for(int b:bobSizes) sb+=b;
+
+        int diff = (sb-sa)/2;
+        Set<Integer> set = new HashSet<>();
+        for(int b:bobSizes) set.add(b);
+
+        for(int a:aliceSizes){
+            if(set.contains(a+diff)){
+                return new int[]{a, a+diff};
+            }
+        }
+        return new int[]{};
+    }
+}
