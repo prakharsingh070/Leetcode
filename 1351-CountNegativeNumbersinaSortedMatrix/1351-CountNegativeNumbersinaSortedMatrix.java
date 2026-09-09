11e007385579f07c0118adb34730e687383a0ec2
Prakhar Pratap Singh <pratapprakhar070@gmail.com>
2026-09-09 11:20:56 +0530

    File updated at 9/9/2026, 11:20:57 AM

--- 1351-CountNegativeNumbersinaSortedMatrix/1351-CountNegativeNumbersinaSortedMatrix.java
+++ 1351-CountNegativeNumbersinaSortedMatrix/1351-CountNegativeNumbersinaSortedMatrix.java
@@ -1,12 +1,19 @@
-// Last updated: 9/9/2026, 11:15:08 AM
-1class Solution {
-2    public int countNegatives(int[][] grid) {
-3        int c=0;
-4        for(int i=0;i<grid.length;i++){
-5            for(int j=0;j<grid[0].length;j++){
-6                if(grid[i][j]<0)c++;
-7            }
-8        }
-9        return c;
-10    }
-11}
+// Last updated: 9/9/2026, 11:20:57 AM
+class Solution {
+    public int countNegatives(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        int count = 0;
+        int row = 0, col = n - 1;
+
+        while (row < m && col >= 0) {
+            if (grid[row][col] < 0) {
+                count += m - row;
+                col--;
+            } else {
+                row++;
+            }
+        }
+
+        return count;
+    }
+}
