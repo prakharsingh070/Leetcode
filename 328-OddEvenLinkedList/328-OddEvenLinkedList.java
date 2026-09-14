0daf7a16ef30f069e781a57926e2d5c667e1c470
Prakhar Pratap Singh <pratapprakhar070@gmail.com>
2026-09-14 22:02:01 +0530

    File updated at 9/14/2026, 10:02:00 PM

--- 328-OddEvenLinkedList/328-OddEvenLinkedList.java
+++ 328-OddEvenLinkedList/328-OddEvenLinkedList.java
@@ -1,4 +1,4 @@
-// Last updated: 9/14/2026, 9:43:54 PM
+// Last updated: 9/14/2026, 10:02:00 PM
 1/**
 2 * Definition for singly-linked list.
 3 * public class ListNode {
@@ -10,18 +10,37 @@
 9 * }
 10 */
 11class Solution {
-12    public ListNode oddEvenList(ListNode head) {
-13        if(head==null || head.next==null) return head;
-14        ListNode odd = head;
-15        ListNode even = head.next;
-16        ListNode evenhead = even;
-17        while(even!=null && even.next!=null){
-18            odd.next = even.next;
-19            odd = odd.next;
-20            even.next = even.next.next;
-21            even = even.next;
-22        }
-23        odd.next = evenhead;
-24        return head;
-25    }
-26}
+12    public ListNode sortList(ListNode head) {
+13        if(head == null || head.next == null) return head;
+14         ListNode prev = null, slow = head, fast = head;
+15         while(fast!=null && fast.next!=null){
+16            prev = slow;
+17            slow = slow.next;
+18            fast = fast.next.next;
+19        }
+20        prev.next = null;
+21        ListNode list1 = sortList(head);
+22        ListNode list2 = sortList(slow);
+23        return merge(list1,list2);
+24    }
+25
+26    public ListNode merge(ListNode list1, ListNode list2){
+27        ListNode dummy = new ListNode(0);
+28        ListNode curr = dummy;
+29
+30        while(list1 !=null && list2!=null){
+31            if(list1.val<list2.val){
+32                curr.next = list1;
+33                list1 = list1.next;
+34            }
+35            else{
+36                curr.next = list2;
+37                list2 = list2.next;
+38            }
+39            curr = curr.next;
+40        }
+41        if(list1 != null) curr.next = list1;
+42        if(list2 != null) curr.next = list2;
+43        return dummy.next;
+44    }
+45}
