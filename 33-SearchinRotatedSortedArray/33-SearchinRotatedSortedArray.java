8d08b754f30488a9f596452e5d002161bff47498
Prakhar Pratap Singh <pratapprakhar070@gmail.com>
2026-09-17 13:42:22 +0530

    File updated at 9/17/2026, 1:42:21 PM

--- 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.java
+++ 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.java
@@ -1,31 +1,13 @@
-// Last updated: 9/12/2026, 10:07:51 PM
+// Last updated: 9/17/2026, 1:42:21 PM
 1class Solution {
-2    public int search(int[] nums, int target) {
-3        int start = 0, end = nums.length - 1;
+2    public int findMin(int[] nums) {
+3        int min = Integer.MAX_VALUE;
 4
-5        while (start <= end) {
-6            int mid = start + (end - start) / 2;
-7
-8            if (nums[mid] == target) return mid;
-9
-10            // Check if left part is sorted
-11            if (nums[mid] >= nums[start]) {
-12                if (target >= nums[start] && target < nums[mid]) {
-13                    end = mid - 1; // move left
-14                } else {
-15                    start = mid + 1; // move right
-16                }
-17            } 
-18            // Right part is sorted
-19            else {
-20                if (target > nums[mid] && target <= nums[end]) {
-21                    start = mid + 1; // move right
-22                } else {
-23                    end = mid - 1; // move left
-24                }
-25            }
-26        }
-27
-28        return -1;
-29    }
-30}
+5        for(int i=0;i<nums.length;i++){
+6            if(nums[i]<=min){
+7                min = nums[i];
+8            }
+9        }
+10        return min;
+11    }
+12}
